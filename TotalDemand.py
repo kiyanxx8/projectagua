be9eb13ebf7db8871647c228a0af8821f276b1df
kiyanx <kizala@ethz.ch>
2024-10-30 20:20:29 +0100

    yeye

--- TotalDemand.py
+++ TotalDemand.py
@@ -2,4 +2,4 @@ def totaldemand(inhabitants):
     Wpriv = 0.073  # L/person/year, water demand for private use
     Wrest = 0.0456  # L/industry/year, water demand for industry
 
-    return inhabitants * Wpriv + inhabitants * Wind
+    return inhabitants * Wpriv + inhabitants * Wrest
--- totalsupply.py
+++ totalsupply.py
@@ -1,4 +1,4 @@
 def totsup(water_act, rainfall, leakage):
-    water_min = 1000 #ML
+    water_min = 1000 #ML, idk the real value
 
     return water_act + rainfall - leakage - water_min
